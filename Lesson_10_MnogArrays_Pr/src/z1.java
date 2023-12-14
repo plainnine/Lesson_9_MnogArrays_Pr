f666af207f07be36241d1853cc60d5956fb9236d
plainnine <gogl.bycl@gmail.com>
2023-12-14 21:47:03 +0200

    Add files via upload

--- Lesson_10_MnogArrays_Pr/src/z1.java
+++ Lesson_10_MnogArrays_Pr/src/z1.java
@@ -12,31 +12,42 @@ public class z1 {
         String s1 = "";
         //объявляется переменная типа массив с именем mas
 
-     /*  mas01[0] = 1;
+        mas01[0] = 1;
         mas01[1] = 2;
-        mas01[2] = 2;
-        mas01[3] = 3;
-        mas01[4] = 4;
-        mas02[0] = 4;
-        mas02[1] = 5;
-        mas02[2] = 6;
-        mas02[3] = 7;
-        mas02[4] = 8;
-*/
+        mas01[2] = 3;
+        mas01[3] = 4;
+        mas01[4] = 5;
+
+        mas02[0] = 9;
+        mas02[1] = 6;
+        mas02[2] = 7;
+        mas02[3] = 2;
+        mas02[4] = 9;
+
         s1 = "mas01 ";
         for (int i = 0; i < mas01.length; i++) {
-           mas01[i] = (int) (1 + Math.random() * 10 );
-            s1 = s1 +"№ "+i+": "+ mas01[i] + " | ";
+          // mas01[i] = (int) (1 + Math.random() * 10 );
+            s1 = s1 +"№_"+i+": "+ mas01[i] + " | ";
         }
         System.out.println(s1);
 
         s1 = "mas02 ";
         for (int i = 0; i < mas02.length; i++) {
-            mas02[i] = (int) (1 + Math.random() * 10 );
-            s1 = s1 +"№ "+i+": "+ mas02[i] + " | ";
+         //   mas02[i] = (int) (1 + Math.random() * 10 );
+            s1 = s1 +"№_"+i+": "+ mas02[i] + " | ";
 
         }
         System.out.println(s1);
+
+        int[] mas1 = new int[mas01.length+mas02.length];
+        int count1 = 0;
+        for (int i = 0; i < mas01.length; i++) {
+            mas1[i] =  mas01[i];
+            count1++;
+        }
+        for (int j : mas02) {
+            mas1[count1++] = j;
+        }
 
         Scanner scan = new Scanner(System.in);
         System.out.println("\n z1 | Введите c клавиатуры вариант:\n");
@@ -44,97 +55,76 @@ public class z1 {
         switch (slct) {
             case 1: {
                 System.out.println("1) Сформировать третий массив, содержащий элементы обоих массивов;");
-                int[] mas1 = new int[10];
+
                 s1 = "mas1 ";
-                for (int i = 0; i < mas01.length; i++) {
-                    mas1[i] =  mas01[i];
-                }
-                for (int i = 0; i < mas02.length; i++) {
-                    mas1[i + mas01.length] =  mas02[i];
-                }
                 for (int i = 0; i < mas1.length; i++) {
 
-                    s1 = s1 + "№ "+i+": " + mas1[i] + " | ";
+                    s1 = s1 + "№_"+i+": " + mas1[i] + " | ";
                 }
                 System.out.println("1) "+s1);
-
             }
             break;
             case 2: {
                 System.out.println("2) Сформировать третий массив, содержащий элементы обоих массивов без повторений;");
-                int[] mas2 = new int[10];
+                int[] mas2 = new int[mas1.length];
                 s1 = "mas2 ";
-                boolean LogProverkaMas01 = true;
-                boolean LogProverkaMas02 = true;
-               int i0 = 0;
+
+                boolean LogProverkaMas2 = true;
                 for (int i = 0; i < mas2.length; i++) { //mas2 массив пуст
-                    for (int j1 : mas01) { //mas01 в массиве повторы
-                        for (int i2 = 0; i2 < i; i2++) {
-                            //mas2 массив уже не пуст
-                            if (mas2[i2] == j1) {
-                                LogProverkaMas01 = false;
+                    for (int j1 = 0; j1 < mas1.length; j1++) {
+                        for (int j2 = 0; j2 < mas1.length; j2++) {
+                            if ((mas1[j2] == mas1[j1])&&(j1!=j2)) {//mas2 поиск уникальных
+                                LogProverkaMas2 = false;
                                 break;
                             }
-
                         }
-                        if (LogProverkaMas01) {
-                            mas2[i] = j1;
-                            s1 = s1 + "№ "+i+": " + mas2[i] + " | ";
-                            i0++; //
+                        for (int i2 = 0; i2 < i; i2++) { //mas2 массив уже не пуст
+                            if (mas2[i2] == mas1[j1]) {
+                                LogProverkaMas2 = false;
+                                break;
+                            }
+                        }
+                        if (LogProverkaMas2) {
+                            mas2[i] = mas1[j1];
+                            s1 = s1 + "№_"+i+": " + mas2[i] + " | ";
                             break;
                         }
-                        LogProverkaMas01 = true;
+                        LogProverkaMas2 = true;
                     }
-
-                } // Запись уникальных чисел из 1 массива
-                for (int i = i0; i < mas2.length; i++) {
-                 for (int j2 : mas02) { //mas02 в массиве повторы
-                    for (int i2 = 0; i2 < i; i2++) {
-                        //mas2 массив уже не пуст
-                        if (mas2[i2] == j2) {
-                            LogProverkaMas02 = false;
-                            break;
-                        }
-                    }
-                    if (LogProverkaMas02) {
-                        mas2[i] = j2;
-                        s1 = s1 + "№ "+i+": " + mas2[i] + " | ";
-                        break;
-                    }
-                     LogProverkaMas02 = true;
-                }}
-                System.out.println("\n 2) "+s1 + " ");
-
+                } // Запись уникальных чисел из массива
+                System.out.println("\n 2) "+s1);
             }
             break;
             case 3: {
                 System.out.println("3) Сформировать третий массив, содержащий элементы общие для двух массивов;");
-                int[] mas3 = new int[10];
-                boolean LogProverkaMas3 = true;
                 s1 = "mas3 ";
-                for (int i = 0; i < mas3.length; i++) { //mas3 массив пуст
+                int[] mas3 = new int[mas1.length];
+                boolean LogProverkaMas3 = true;
+                boolean LogEqualMas3 = true;
+                for (int i = 0; i < mas3.length; i++) { //mas2 массив пуст
+                    for (int j1 = 0; j1 < mas1.length; j1++) {
 
-                        for (int i2 = 0; i2 < i; i2++) {
-                            //mas3 массив уже не пуст
-                            if (mas3[i2] == j1) {
+                        for (int j2 = 0; j2 < mas1.length; j2++) { //mas2 поиск повторов
+                            if ((mas1[j2] == mas1[j1])&&(j1!=j2)) {
+                                LogEqualMas3 = false;
+                                break;
+                            }}
+                        for (int i2 = 0; i2 < i; i2++) { //mas2 массив уже не пуст
+                            if (mas3[i2] == mas1[j1]) {
                                 LogProverkaMas3 = false;
                                 break;
-                            }
-
-                        }
-
-                        if (LogProverkaMas3) {
-                            mas3[i] = j1;
-                            s1 = s1 + "№ "+i+": " + mas3[i] + " | ";
-
+                            }}
+                        if ((LogProverkaMas3)&&(!LogEqualMas3)) {
+                            mas3[i] = mas1[j1];
+                            s1 = s1 + "№_"+i+": " + mas3[i] + " | ";
                             break;
                         }
                         LogProverkaMas3 = true;
+                        LogEqualMas3 = true;
 
+                    }} // Запись уникальных чисел из массива
 
-                }
-                System.out.println("\n 3) "+s1 + " ");
-                
+                System.out.println("\n 3) "+s1);
             }
             break;
             default:
